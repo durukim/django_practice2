fe5dfbf7f3517b05c9f303ee783c7cb215f71283
durukim <kimduru@naver.com>
2022-06-03 16:42:00 +0900

    feat : add create

--- costory/posts/views.py
+++ costory/posts/views.py
@@ -1,6 +1,7 @@
 from difflib import context_diff
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import PostForm
 # Create your views here.
 
 
@@ -8,3 +9,20 @@ def post_list(request):
     posts = Post.objects.all()
     context = {"posts": posts}
     return render(request, 'posts/post_list.html', context)
+
+
+def post_detail(request, post_id):
+    post = Post.objects.get(id=post_id)
+    context = {"post": post}
+    return render(request, 'posts/post_detail.html', context)
+
+
+def post_create(request):
+    if request.method == 'POST':
+        post_form = PostForm(request.POST)
+        if post_form.is_valid():
+            new_post = post_form.save()
+            return redirect('post-detail', post_id=new_post.id)
+    else:
+        post_form = PostForm()
+    return render(request, 'posts/post_form.html', {'form': post_form})
